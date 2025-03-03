c4f02b3ed5db151c13c02e93d7b85e5a8dac9f99
George Vlahavas <vlahavas@gmail.com>
2025-03-03 11:31:45 +0200

    Publish on Mon Mar  3 11:31:45 EET 2025

--- index.search.js
+++ index.search.js
@@ -433,8 +433,8 @@ var relearn_search_index = [
   },
   {
     "breadcrumb": "Salix Documentation \u003e FAQ",
-    "content": "Salix 14.1 and newer For Salix 14.1 and newer there is no root user. You can log in as user one and the password is also one. User one can use the sudo command to execute commands as the superuser.\nSalix 14.0 and earlier For Salix 14.0 and earlier:\nThe Salix Live root password is live. The Salix Live non-root username is one with no password. ",
-    "description": "Salix 14.1 and newer For Salix 14.1 and newer there is no root user. You can log in as user one and the password is also one. User one can use the sudo command to execute commands as the superuser.\nSalix 14.0 and earlier For Salix 14.0 and earlier:\nThe Salix Live root password is live. The Salix Live non-root username is one with no password. ",
+    "content": "You can log in SalixLive as user one. The password is also one. User one can use the sudo command to execute commands as the superuser.\nThe root user is disabled by default and therefore you can’t log in as root.\nSalix 14.0 and earlier These are now old and unsupported releases of Salix, but just in case you happen to come across one, here is the relevant information:\nYou log in as user one. If you need to make any changes to the system, you need to become root. The Salix Live non-root username is one with no password. The Salix Live root password is live. ",
+    "description": "You can log in SalixLive as user one. The password is also one. User one can use the sudo command to execute commands as the superuser.\nThe root user is disabled by default and therefore you can’t log in as root.\nSalix 14.0 and earlier These are now old and unsupported releases of Salix, but just in case you happen to come across one, here is the relevant information:\nYou log in as user one.",
     "tags": [],
     "title": "SalixLive Passwords",
     "uri": "/faq/salixlive-passwords/"
